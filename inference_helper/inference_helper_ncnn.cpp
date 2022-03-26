fd3cf29805a322c975774e0160f8d2988f51d441
iwatake2222 <take.iwiw2222@gmail.com>
2022-03-26 21:09:28 +0900

    [ncnn] Add custom layer for YoloV5Focus

--- inference_helper/inference_helper_ncnn.cpp
+++ inference_helper/inference_helper_ncnn.cpp
@@ -37,8 +37,58 @@ limitations under the License.
 #define PRINT_E(...) INFERENCE_HELPER_LOG_PRINT_E(TAG, __VA_ARGS__)
 
 /*** Function ***/
+/* Reference: https://github.com/Tencent/ncnn/blob/master/examples/yolox.cpp */
+class YoloV5Focus : public ncnn::Layer
+{
+public:
+    YoloV5Focus()
+    {
+        one_blob_only = true;
+    }
+
+    virtual int forward(const ncnn::Mat& bottom_blob, ncnn::Mat& top_blob, const ncnn::Option& opt) const
+    {
+        int w = bottom_blob.w;
+        int h = bottom_blob.h;
+        int channels = bottom_blob.c;
+
+        int outw = w / 2;
+        int outh = h / 2;
+        int outc = channels * 4;
+
+        top_blob.create(outw, outh, outc, 4u, 1, opt.blob_allocator);
+        if (top_blob.empty())
+            return -100;
+
+#pragma omp parallel for num_threads(opt.num_threads)
+        for (int p = 0; p < outc; p++)
+        {
+            const float* ptr = bottom_blob.channel(p % channels).row((p / channels) % 2) + ((p / channels) / 2);
+            float* outptr = top_blob.channel(p);
+
+            for (int i = 0; i < outh; i++)
+            {
+                for (int j = 0; j < outw; j++)
+                {
+                    *outptr = *ptr;
+
+                    outptr += 1;
+                    ptr += 2;
+                }
+
+                ptr += w;
+            }
+        }
+
+        return 0;
+    }
+};
+DEFINE_LAYER_CREATOR(YoloV5Focus)
+
 InferenceHelperNcnn::InferenceHelperNcnn()
 {
+    custom_ops_.clear();
+    custom_ops_.push_back(std::pair<const char*, const void*>("YoloV5Focus", (const void*)YoloV5Focus_layer_creator));
     num_threads_ = 1;
 }
 
